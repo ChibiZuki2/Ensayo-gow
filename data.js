7a959aee8f3cbe689f4c4758e71bb5f9f66aab1e
ChibiZuki2 <m.morenoastete@gmail.com>
2026-07-01 23:30:28 -0400

    Agrega fanart: Sin palabras dajdjd

--- data.js
+++ data.js
@@ -67,6 +67,7 @@ const CHIBI_FRASES = [
    🖼️ IMÁGENES / FANARTS
    ============================ */
 const IMAGENES = [
+      { src:"imagenes/1782963004665-cc6f65be-a106-420c-a83f-e5a0a5d351fa.webp", titulo:"Sin palabras dajdjd", artista:"coso", categoria:"fanart", fecha:"2026-07-02", placeholder:"🎨", nota:"Drelfi, entrapta y yo", destacado:true },
       { src:"imagenes/1782962956126-1c67dd61-4a0f-4c1f-b117-6a99bb2b8172.webp", titulo:"San gowi", artista:"coso", categoria:"fanart", fecha:"2026-07-02", placeholder:"🎨", nota:"prueba", destacado:false },
       { src:"imagenes/1782962928224-ba7f692b-b4b5-429b-b839-efafa3052f1f-1-.webp", titulo:"Vitral coso", artista:"coso", categoria:"fanart", fecha:"2026-07-02", placeholder:"🎨", nota:"probando", destacado:false },
       { src:"imagenes/1782962583054-ae858789-ad9c-442d-96a4-5479403fcf2a.webp", titulo:"Probando 1", artista:"coso", categoria:"fanart", fecha:"2026-07-02", placeholder:"🎨", nota:"", destacado:true },
