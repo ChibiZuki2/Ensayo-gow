70babd8a732246f4783caee4c2ea714a311d1c4a
ChibiZuki2 <m.morenoastete@gmail.com>
2026-07-02 05:17:17 -0400

    Elimina fanart: Ejemplo emote

--- data.js
+++ data.js
@@ -73,7 +73,6 @@ const IMAGENES = [
       { src:"imagenes/1782983755963-ba7f692b-b4b5-429b-b839-efafa3052f1f-1-.webp", titulo:"Vitral coso", artista:"coso", categoria:"fanart", fecha:"2026-07-02", placeholder:"🎨", nota:"con la yepita", destacado:true },
       { src:"imagenes/1782983719068-proyecto-gowi-1.webp", titulo:"Uno de mis primeros fanarts", artista:"Chibi", categoria:"fanart", fecha:"2026-07-02", placeholder:"🎨", nota:"Fue uno de los primeros fan art que me hicieron", destacado:true },
   { src:"imagenes/ejemplo01.jpg", titulo:"Ejemplo fanart 1",  artista:"@artista",     categoria:"fanart", fecha:"2026-05-01", placeholder:"🎨", nota:"", destacado:false },
-  { src:"imagenes/ejemplo02.jpg", titulo:"Ejemplo emote",     artista:"@emoteartist", categoria:"emotes", fecha:"2026-05-10", placeholder:"✨", nota:"", destacado:false },
 ];
 
 /* ============================
