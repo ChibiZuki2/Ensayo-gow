a8672607b85f6c80c154b56720b80a1c8e464011
ChibiZuki2 <m.morenoastete@gmail.com>
2026-07-03 16:16:47 -0400

    Agrega icono oveja: 1783109805290-oveja1.png

--- data.js
+++ data.js
@@ -76,6 +76,7 @@ const IMAGENES = [
    🎮 ICONOS DE OVEJA (calificaciones de juegos)
    ============================ */
 const OVEJA_IMGS = [
+  "imagenes/ovejas/1783109805290-oveja1.png",
   "imagenes/ovejas/oveja2.png",
   "imagenes/ovejas/oveja3.png",
   "imagenes/ovejas/oveja4.png",
